dc2f3005653f03652210f1abec116d393c239ca2
Jordan Emslie <jordems@gmail.com>
2020-02-21 16:51:23 -0800

    Removing Old Files

--- app/seedindicatorbuilder.py
+++
@@ -1,6 +0,0 @@
-# Class Purpose: This class will build the seed indicator database
-# * Will take the data from the phdb database and
-#   indicator techniques to build scenarios as
-#   samples of common situations and whether it was posistive or negative on the pricing.
-#   Displaying weather an indicator says to buy, sell, or do nothing. and associated risk involved based on seeds
-# ** May need to change **
--- app/tcdsfiller.py
+++
@@ -1,3 +0,0 @@
-# This Class is used to Fill the Temporary Current Data Set (TCDS) into the Mongodb
-# * Should Contain the class to grab data from previous minute, hour, day and month
-# * Also, should open be able to open a websocket to collect realtime minute, hour, day, and month data
