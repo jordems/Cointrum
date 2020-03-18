321d4407b3bc2d3def3abe416c0374ec2d348324
Jordan Emslie <jordems@gmail.com>
2020-03-17 22:20:48 -0700

    Fix Null References

--- cointrum-frontend/src/pages/home/index.tsx
+++ cointrum-frontend/src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button, TextField, WithStyles } from "@material-ui/core";
+import { WithStyles } from "@material-ui/core";
 import { ConnectedProps } from "react-redux";
 
 import { styles, wrapStyles } from "./styles";
@@ -10,31 +10,10 @@ type HomePageProps = ConnectedProps<typeof connector> &
   WithStyles<typeof styles>;
 
 class HomePage extends React.Component<HomePageProps> {
-  readonly state: any = {
-    message: ""
-  };
-
-  handleAddMessage = () => {
-    this.props.addMessagetoTest(this.state.message);
-  };
-  handleRemoveMessage = () => {
-    this.props.removeMessagefromTest();
-  };
-
-  handleEditText = (e: any) => {
-    this.setState({ message: e.target.value });
-  };
-
   render() {
     return (
       <div>
         <h1>Home</h1>
-        <Button onClick={this.handleAddMessage}>Add Message</Button>
-        <TextField onChange={this.handleEditText} />
-        {this.props.test.messages.map((message: any, idx: any) => (
-          <div key={idx}>{message}</div>
-        ))}
-        <Button onClick={this.handleRemoveMessage}>Remove Message</Button>
       </div>
     );
   }
--- cointrum-frontend/src/pages/home/redux.tsx
+++ cointrum-frontend/src/pages/home/redux.tsx
@@ -2,16 +2,6 @@ import { connect } from "react-redux";
 
 import { AppState } from "store";
 
-import {
-  addMessagetoTest,
-  removeMessagefromTest
-} from "store/learning/actions/test.action";
+const mapStateToProps = (state: AppState) => ({});
 
-const mapStateToProps = (state: AppState) => ({
-  test: state.learning.test
-});
-
-export const connector = connect(mapStateToProps, {
-  addMessagetoTest,
-  removeMessagefromTest
-});
+export const connector = connect(mapStateToProps, {});
